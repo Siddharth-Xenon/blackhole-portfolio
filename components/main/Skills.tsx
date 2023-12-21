8f3b9a60fc3e7fb02e489200cf4b81b100bd6d20
Siddharth-Xenon <sidsolanki920@gmail.com>
2023-12-21 21:10:50 +0530

    personalised

--- components/main/Skills.tsx
+++ components/main/Skills.tsx
@@ -51,7 +51,7 @@ const Skills = () => {
             index={index}
           />
         ))}
-      </div>
+      {/* </div>
       <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
         {Full_stack.map((image, index) => (
           <SkillDataProvider
@@ -72,7 +72,7 @@ const Skills = () => {
             height={image.height}
             index={index}
           />
-        ))}
+        ))} */}
       </div>
 
       <div className="w-full h-full absolute">
